fa6ee82882c2bb8ec411006ea418a68018e599c1
vdongdn <vietdongbkdn@gmail.com>
2023-09-19 23:15:20 +0700

    fix

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,5 +1,3 @@
-const Place = require("../models/Place");
-
 class SiteController {
 
     //[GET] /
@@ -7,20 +5,5 @@ class SiteController {
         res.render('home')
     }
 
-    //[GET] /search
-    search(req, res) {
-        res.render('search', { obj: req.query })
-    }
-
-    //[POST] /search
-    async doSearch(req, res) {
-        try {
-            const p = await Place.findOne({ title: { $regex: req.body.q, $options: 'i' } });
-            res.render('search', { result: p.title })
-        } catch (error) {
-            res.render('search', { result: 'Nỏ tìm thấy' })
-            res.render('search', { result: 'Nỏ tìm thấy' })
-        }
-    }
 }
 module.exports = new SiteController;
